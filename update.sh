df25efea2283e4f3d17d00061bc9254b8bc8c570
Joshua <joshuagibeon7719@gmail.com>
2017-09-26 13:55:26 +0700

    Add fetching procedure

--- update.sh
+++ update.sh
@@ -28,3 +28,23 @@ then
 else
 	echo [OK]
 fi
+
+###Grab Procedure
+
+##Grab http://ukdw.ac.id/e-class/id/kelas/index
+echo -n Grabbing http://ukdw.ac.id/e-class/id/kelas/index...
+curl -s 'http://ukdw.ac.id//e-class/id/kelas/index' \
+-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36' \
+-b cookies.txt \
+--compressed -o index.txt
+
+##Check if grabbing is failed
+CHECK="$?"
+if [ $CHECK -ne 0 ]
+then
+	echo [FAILED]
+	echo error code $CHECK
+	exit
+else
+	echo [OK]
+fi
