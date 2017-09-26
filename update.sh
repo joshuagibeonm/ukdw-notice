3ef89c33b34ffc8714bb20a14c424d5d1e64e5cc
Joshua Gibeon <32270425+joshuagibeonm@users.noreply.github.com>
2017-09-26 10:24:04 +0700

    Create update.sh
    With login procedure to www.ukdw.ac.id

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#filename: update.sh
+
+###Login Procedure
+
+##Read NIM and Password
+echo Welcome to UKDW-notice
+echo -n NIM: ;read NIM
+echo -n Password: ;
+
+stty -echo;       #to hide user's input
+read PASSWORD
+stty echo;        #to show user's input
+
+##Login to UKDW
+echo -n Login to UKDW as $NIM...
+curl -s 'http://ukdw.ac.id/id/home/do_login' \
+-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36' \
+--data "return_url=http%3A%2F%2Fukdw.ac.id%2Fe-class%2Fid%2Fkelas%2Findex&id=${NIM}&password=${PASSWORD}" \
+--compressed -c cookies.txt
+
+##Check if login is failed
+CHECK="$?"
+if [ $CHECK -ne 0 ]
+then
+	echo Failed with error code $CHECK
+	exit
+else
+	echo [OK]
+fi
