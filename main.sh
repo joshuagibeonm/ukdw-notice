e88f00c3449414c799ebe1a118f56c68bfe1d844
Ordinal43 <bryan.ramaputra@ti.ukdw.ac.id>
2017-10-13 04:42:07 -0700

    Add loading screen dialog

--- main.sh
+++ main.sh
@@ -26,7 +26,7 @@ function login-page {
 	done
 
 	##Login to UKDW
-	echo -n Login to UKDW as $NIM...
+	dialog --infobox "Login to UKDW as $NIM..." 8 50
 	curl -s 'http://ukdw.ac.id/id/home/do_login' \
 	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36' \
 	--data "return_url=http%3A%2F%2Fukdw.ac.id%2Fe-class%2Fid%2Fkelas%2Findex&id=${NIM}&password=${PASSWORD}" \
@@ -35,12 +35,9 @@ function login-page {
 	CHECK="$?"
 	if [ $CHECK -ne 0 ]
 	then
-		echo Failed with error code $CHECK
+		dialog --msgbox "Login failed with error code $CHECK" 8 50
 		exit
-	else
-		echo [OK]
 	fi
-
 
 	update
 }
@@ -72,7 +69,7 @@ function show-pengumuman {
 		((INDEX++))
 	done
 
-	while [ true ]
+	while true
 	do
 		RESPON=$(dialog --cancel-label "Back" --title "Menu Pengumuman" \
 		--menu "" 20 80 100 \
@@ -115,36 +112,31 @@ function update {
 	###Grab Procedure
 
 	##Grab http://ukdw.ac.id/e-class/id/kelas/index
-	echo -n Grabbing http://ukdw.ac.id/e-class/id/kelas/index...
-	curl -s 'http://ukdw.ac.id//e-class/id/kelas/index' \
-	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36' \
-	-b cookies.txt \
-	--compressed -o index.txt
+
+		dialog --infobox "Grabbing http://ukdw.ac.id/e-class/id/kelas/index..." 8 50
+		curl -s 'http://ukdw.ac.id//e-class/id/kelas/index' \
+		-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36' \
+		-b cookies.txt \
+		--compressed -o index.txt
 
 	##Check if grabbing is failed
 	CHECK="$?"
 	if [ $CHECK -ne 0 ]
 	then
-		echo [FAILED]
-		echo error code $CHECK
+		dialog --msgbox "Grabbing failed with Error code: $CHECK"
 		exit
-	else
-		echo [OK]
 	fi
 
 	###Filtering Tugas and Pengumuman link
-	echo -n Filtering Pengumuman link and Tugas Link...
+	dialog --infobox "Filtering Pengumuman link and Tugas Link..." 8 50
 	grep -Eoi '<a class="menu mc"[^>]+>' index.txt | grep -Eo 'href="[^\"]+"' > link.txt
 
 	##Check if link.txt is empty or grep is failed
 	CHECK="$?"
-	if [ -s link.txt ] && [ $CHECK -eq 0 ]
+	if [ ! -s link.txt ] || [ ! $CHECK -eq 0 ]
 	then
-	    echo [OK]
-	else
-	    echo [FAILED]
-		echo error code $CHECK
-	    exit
+		dialog --msgbox "Filtering failed with Error code: $CHECK" 8 50
+		exit
 	fi
 
 	##Check whether user wants to go into main menu
